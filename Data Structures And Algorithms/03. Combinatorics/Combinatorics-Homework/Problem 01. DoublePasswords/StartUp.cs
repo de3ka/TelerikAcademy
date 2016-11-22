ce7df5af52f06624bdd42e03c9df70bd77eac2cc
de3ka <derketo@abv.bg>
2016-11-23 00:06:46 +0200

    up

---
+++ Data Structures And Algorithms/03. Combinatorics/Combinatorics-Homework/Problem 01. DoublePasswords/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem_01.DoublePasswords
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            
+            var numberOfStars = 0;
+            foreach (var symbol in input)
+            {
+                if (symbol == '*')
+                {
+                    ++numberOfStars;
+                }
+            }
+            
+            long result = 1;
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                result *= 2;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Data Structures And Algorithms/03. Combinatorics/Combinatorics-Homework/Problem 02. ColorfulRabbits/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_02.ColorfulRabbits
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfAskedRabbits = int.Parse(Console.ReadLine());
+
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < numberOfAskedRabbits; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+
+                if (dict.ContainsKey(number))
+                {
+                    ++dict[number];
+                }
+                else
+                {
+                    dict[number] = 1;
+                }
+            }
+            
+            long rabbits = 0;
+            foreach (var pair in dict)
+            {
+                var multiplier = pair.Value / (pair.Key + 1);
+                if (pair.Value % (pair.Key + 1) != 0)
+                {
+                    ++multiplier;
+                }
+
+                rabbits += (pair.Key + 1) * multiplier;
+            }
+
+            Console.WriteLine(rabbits);
+        }
+    }
+}
